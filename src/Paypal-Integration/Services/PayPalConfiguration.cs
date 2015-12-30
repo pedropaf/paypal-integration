eb5fe0049460e3d99e57d503a06b226ad96759b9
pedro.alonso <pedro.alonso@photonube.com>
2015-12-30 16:59:09 +0100

    paypal client secret added.

--- src/Paypal-Integration/Services/PayPalConfiguration.cs
+++ src/Paypal-Integration/Services/PayPalConfiguration.cs
@@ -26,35 +26,27 @@ namespace Paypal_Integration.Services
         private static string GetAccessToken()
         {
             // ###AccessToken
-            // Retrieve the access token from
-            // OAuthTokenCredential by passing in
+            // Retrieve the access token from OAuthTokenCredential by passing in
             // ClientID and ClientSecret
             // It is not mandatory to generate Access Token on a per call basis.
-            // Typically the access token can be generated once and
-            // reused within the expiry window                
-            string accessToken = new OAuthTokenCredential(ClientId, ClientSecret, GetConfig()).GetAccessToken();
+            // Typically the access token can be generated once and reused within the expiry window                
+            string accessToken = new OAuthTokenCredential
+                (ClientId, ClientSecret, GetConfig()).GetAccessToken();
             return accessToken;
         }
 
         // Returns APIContext object
         public static APIContext GetAPIContext(string accessToken = "")
         {
-            // ### Api Context
             // Pass in a `APIContext` object to authenticate 
             // the call and to send a unique request id 
             // (that ensures idempotency). The SDK generates
             // a request id if you do not pass one explicitly. 
-            var apiContext = new APIContext(string.IsNullOrEmpty(accessToken) ? GetAccessToken() : accessToken);
+            var apiContext = new APIContext(string.IsNullOrEmpty(accessToken) ?
+                GetAccessToken() : accessToken);
             apiContext.Config = GetConfig();
-
-            // Use this variant if you want to pass in a request id  
-            // that is meaningful in your application, ideally 
-            // a order id.
-            // String requestId = Long.toString(System.nanoTime();
-            // APIContext apiContext = new APIContext(GetAccessToken(), requestId ));
-
+            
             return apiContext;
         }
-
     }
 }
