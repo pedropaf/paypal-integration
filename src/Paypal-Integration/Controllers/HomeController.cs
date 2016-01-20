5c69ebda3632f16d43262329d64539982cd14bf1
pedro.alonso <pedro.alonso@photonube.com>
2016-01-20 20:05:32 +0100

    paypal webhooks

--- src/Paypal-Integration/Controllers/HomeController.cs
+++ src/Paypal-Integration/Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNet.Mvc;
 using Paypal_Integration.Services;
 using PayPal.Api;
+using System.Collections.Specialized;
+using Microsoft.AspNet.Http;
+using Newtonsoft.Json.Linq;
 
 namespace Paypal_Integration.Controllers
 {
@@ -91,9 +94,68 @@ namespace Paypal_Integration.Controllers
         }
         #endregion
 
+        #region WebHooks
+        public IActionResult Webhook()
+        {
+            // The APIContext object can contain an optional override for the trusted certificate.
+            var apiContext = PayPalConfiguration.GetAPIContext();
+
+            // Get the received request's headers
+            var requestheaders = HttpContext.Request.Headers;
+
+            // Get the received request's body
+            var requestBody = string.Empty;
+            using (var reader = new System.IO.StreamReader(HttpContext.Request.Body))
+            {
+                requestBody = reader.ReadToEnd();
+            }
+
+            dynamic jsonBody = JObject.Parse(requestBody);
+            string webhookId = jsonBody.id;
+            var ev = WebhookEvent.Get(apiContext, webhookId);
+            
+            // We have all the information the SDK needs, so perform the validation.
+            // Note: at least on Sandbox environment this returns false.
+            // var isValid = WebhookEvent.ValidateReceivedEvent(apiContext, ToNameValueCollection(requestheaders), requestBody, webhookId);
+            
+            switch (ev.event_type)
+            {
+                case "PAYMENT.CAPTURE.COMPLETED":
+                    // Handle payment completed
+                    break;
+                case "PAYMENT.CAPTURE.DENIED":
+                    // Handle payment denied
+                    break;
+                    // Handle other webhooks
+                default:
+                    break;
+            }
+
+            return new HttpStatusCodeResult(200);
+        }
+        #endregion
+
         public string GetBaseUrl()
         {
             return Request.Scheme + "://" + Request.Host;
+        }
+
+        public NameValueCollection ToNameValueCollection(IHeaderDictionary dict)
+        {
+            var nameValueCollection = new NameValueCollection();
+
+            foreach (var kvp in dict)
+            {
+                string value = null;
+                if (!string.IsNullOrEmpty(kvp.Value))
+                {
+                    value = kvp.Value.ToString();
+                }
+
+                nameValueCollection.Add(kvp.Key.ToString(), value);
+            }
+
+            return nameValueCollection;
         }
     }
 }
